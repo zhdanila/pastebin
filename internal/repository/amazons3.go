c3afde6e2072d2fd3a0af0fd36758bd5b3b44a54
zhdanila <daniazhurba2004@gmail.com>
2024-05-21 07:48:56 +0300

    Add Amazons3 connection

--- internal/repository/amazons3.go
+++ internal/repository/amazons3.go
@@ -1,1 +1,39 @@
 package repository
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+type AmazonConfig struct {
+	Region    string
+	AccessKey string
+	SecretKey string
+}
+
+type AmazonDB struct {
+	svc *s3.S3
+}
+
+func NewAmazonDB(config AmazonConfig) (*AmazonDB, error) {
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(config.Region),
+		Credentials: credentials.NewStaticCredentials(config.AccessKey, config.SecretKey, ""),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	svc := s3.New(sess)
+	return &AmazonDB{svc: svc}, nil
+}
+
+func (db *AmazonDB) CheckConnection() error {
+	_, err := db.svc.ListBuckets(nil)
+	if err != nil {
+		return err
+	}
+	return nil
+}
